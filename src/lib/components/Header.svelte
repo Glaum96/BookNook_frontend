c0ee55d3f852b4c7328e6a346bb39df2b9d507da
Haakon Gunleiksrud <haakon.gunleiksrud@sparebank1.no>
2025-02-13 14:31:18 +0100

    Use users name in header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,24 +1,47 @@
 <script lang="ts">
-	import { isAuthenticated, logOut } from '../stores/auth'
 	import { goto } from '$app/navigation'
+	import { onMount } from 'svelte'
+	import { isAuthenticated, logOut } from '../../stores/auth'
+	import type { User } from '../../types/User'
 
-	function navigateTo(url: string) {
+	const navigateTo = (url: string) => {
 		console.log('Navigating to:', url) // Debugging
 		goto(url)
 	}
 
 	// Function to handle logout
-	function handleLogOut() {
+	const handleLogOut = () => {
 		logOut()
 		navigateTo('/login')
 	}
+
+	const getUserNameText = (userName: string | null) => {
+		if (!userName) {
+			return 'Min side'
+		}
+
+		if (userName.endsWith('s')) {
+			return `${userName}' side`
+		} else {
+			return `${userName}s side`
+		}
+	}
+
+	let userName: string | null = null
+	onMount(() => {
+		const user = localStorage.getItem('user')
+		if (user) {
+			const userObj: User = JSON.parse(user)
+			userName = userObj.name
+		}
+	})
 </script>
 
 <header>
 	<nav>
 		<a href="/" on:click|preventDefault={() => navigateTo('/')}>Seilduksgata 6B</a>
 		{#if $isAuthenticated}
-			<a href="/minside" on:click|preventDefault={() => navigateTo('/minside')}>Min side</a>
+			<a href="/minside" on:click|preventDefault={() => navigateTo('/minside')}>{getUserNameText(userName)}</a>
 			<a href="/admin" on:click|preventDefault={() => navigateTo('/admin')}>Admin</a>
 			<button on:click={handleLogOut} class="nav-link">Logg ut</button>
 		{:else}
